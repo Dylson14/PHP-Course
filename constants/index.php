a9666000c1093fa834ad145b70844922a8fdd4cf
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-13 14:31:48 +0000

    learning about constants

---
+++ constants/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constants</title>
+</head>
+<body>
+    <?php 
+
+        define("PI", 3.14);
+        define("NAME", "Dylson");
+        define("IS_ADMIN", true);
+        /* echo PI;
+        echo NAME; */
+        // echo IS_ADMIN;
+
+        function test(){
+            echo PI;
+        }
+        
+        echo test();
+
+    ?>
+</body>
+</html>
--- scopes/index.php
+++ scopes/index.php
@@ -1,28 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-    
+    <?php
+
     /* $test = "Jake";
     echo $test; */
-    $test = "Jack";
+    // $test = "Jack";
 
-    function myFunction(){
+    /*  function myFunction(){
         // $localVar = "Hello World!";
         // global $test;
 
         // return $test;
         return $GLOBALS["test"];
+    } */
+
+    // echo myFunction();
+    /* echo $localVar;
+    echo $test; */
+
+    function myFunction()
+    {
+        // Declare a static variable
+        static $staticVar = 0;
+
+        // Increment the static variable
+        $staticVar++;
+
+        // Use the static variable
+        return $staticVar;
     }
 
     echo myFunction();
-    /* echo $localVar;
-    echo $test; */
+    echo myFunction();
+
+
     ?>
+
+
 </body>
+
 </html>
