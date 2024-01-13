0fadb2c9f005aeaa4c7a2f78b5fc0d077b9cf4d1
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-13 15:26:03 +0000

    learned about loops in php

--- loops/index.php
+++ loops/index.php
@@ -7,5 +7,33 @@
 </head>
 <body>
     
+    <?php
+    /* $test = 5;
+    for($i = 0; $i <= $test; $i++) {
+        echo "This is iteration number " . $i . "<br>";
+    } */
+
+   /*  $test = 10;
+    do {
+        echo $test . "<br>";
+        $test++;
+    } while ($test < 10);
+ */
+    // $fruits = ["Apple", "Banana", "Orange"];
+    $fruits = ["Apple" => "Red", "Banana" => "Yellow", "Orange" => "Orange"];
+
+    // echo $fruits[0];
+/* 
+    for($i = 0; $i < count($fruits); $i++){
+        echo $fruits[$i] . "<br>";
+
+    } */
+
+    foreach($fruits as $fruit => $color) {
+        echo "This is a " . $fruit . " That has a color of " . $color . "<br>";
+    }
+
+    ?>
+
 </body>
 </html>
