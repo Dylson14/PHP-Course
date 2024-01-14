7fca3a6a262e1e8b20bc5f602a30107c87c6b6f4
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-14 15:36:21 +0000

    learned the UPDATE and DELETE CRUD operators using SQL in php

--- dabatase/index.php
+++ dabatase/index.php
@@ -6,8 +6,27 @@
     <title>Databse PHP</title>
 </head>
 <body>
-    <?php 
+        <h3>Change Account</h3>
         
-    ?>
+        <form action="includes/userupdate.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <br>
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <input type="text" name="email" placeholder="E-Mail">
+            <br>
+            <button>Update</button>
+        </form>
+        
+        <h3>Delete Account</h3>
+
+        <form action="includes/userdelete.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <br>
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <button>Delete</button>
+        </form>
+
 </body>
 </html>
